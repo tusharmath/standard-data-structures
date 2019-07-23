f1a41ee349ac63ec0945e93e19e2c72cd521d4a0
Tushar Mathur <tusharmath@gmail.com>
2019-07-23 11:48:50 +0530

    test(index): update tests for DoublyLinkedList

--- test/index.ts
+++ test/index.ts
@@ -2,10 +2,14 @@ import {assert} from 'chai'
 
 import * as C from '../'
 import {List} from '../src/immutable/list'
+import {DoublyLinkedList} from '../src/mutable/doublyLinkedList'
 import {MaxHeap} from '../src/mutable/maxHeap'
 
 describe('index', () => {
   it('should export all DS', () => {
-    assert.deepStrictEqual(C, {immutable: {List}, mutable: {MaxHeap}})
+    assert.deepStrictEqual(C, {
+      immutable: {List},
+      mutable: {MaxHeap, DoublyLinkedList}
+    })
   })
 })
