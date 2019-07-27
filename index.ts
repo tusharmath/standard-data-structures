7a86482ea69a1a31cb1956807db588d59ffa38bc
Tushar Mathur <tusharmath@gmail.com>
2019-07-27 20:15:42 +0530

    fix(mutable): export LinkedListNode

--- index.ts
+++ index.ts
@@ -2,6 +2,8 @@ import {List} from './src/immutable/list'
 import {DoublyLinkedList} from './src/mutable/doublyLinkedList'
 import {MaxHeap} from './src/mutable/maxHeap'
 
+export {LinkedListNode} from './src/mutable/doublyLinkedList'
+
 /**
  * Namespace for all immutable data-structures
  */
