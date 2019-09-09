099df4e48e0ca33a7601b443ee6577b9360b4aa6
Tushar Mathur <tusharmath@gmail.com>
2019-09-09 21:06:58 +0530

    feat(package): export all dependencies from root

--- index.ts
+++ index.ts
@@ -1,22 +1,5 @@
-import {Either} from './src/immutable/either'
-import {List} from './src/immutable/list'
-import {DoublyLinkedList} from './src/mutable/doublyLinkedList'
-import {MaxHeap} from './src/mutable/maxHeap'
-
+export {Either} from './src/immutable/either'
+export {List} from './src/immutable/list'
+export {DoublyLinkedList} from './src/mutable/doublyLinkedList'
+export {MaxHeap} from './src/mutable/maxHeap'
 export {LinkedListNode} from './src/mutable/doublyLinkedList'
-
-/**
- * Namespace for all immutable data-structures
- */
-export const immutable = {
-  Either,
-  List
-}
-
-/**
- * Namespace for all mutable data-structures
- */
-export const mutable = {
-  DoublyLinkedList,
-  MaxHeap
-}
--- test/mutable/maxHeap.ts
+++ test/mutable/maxHeap.ts
@@ -1,24 +1,24 @@
 import * as assert from 'assert'
 
-import {mutable} from '../..'
+import {MaxHeap} from '../../src/mutable/maxHeap'
 
 describe('maxHeap', () => {
   it('should maintain order', () => {
-    const heap = mutable.MaxHeap.numbers
+    const heap = MaxHeap.numbers
     heap.push(1)
     heap.push(10)
     assert.strictEqual(heap.pop(), 10)
   })
 
   it('should return length', () => {
-    const heap = mutable.MaxHeap.numbers
+    const heap = MaxHeap.numbers
     heap.push(1)
     heap.push(10)
     assert.strictEqual(heap.length, 2)
   })
 
   it('should return top element', () => {
-    const heap = mutable.MaxHeap.numbers
+    const heap = MaxHeap.numbers
     heap.push(1)
     heap.push(10)
     assert.strictEqual(heap.peek, 10)
